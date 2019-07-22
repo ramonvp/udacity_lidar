010b884e279202e3cbc0543e433d8cf782e82105
ramonvp <ramon.viedma@hp.com>
2019-07-22 08:10:19 +0200

    Erase commented code

--- src/processPointClouds.h
+++ src/processPointClouds.h
@@ -22,45 +22,6 @@
 
 #include "kdtree.h"
 
-#if 0
-template<typename PointT>
-class ProcessPointClouds {
-public:
-
-    //constructor
-    ProcessPointClouds();
-    //deconstructor
-    ~ProcessPointClouds();
-
-    void numPoints(typename pcl::PointCloud<PointT>::Ptr cloud);
-
-    typename pcl::PointCloud<PointT>::Ptr FilterCloud(typename pcl::PointCloud<PointT>::Ptr cloud, float filterRes, Eigen::Vector4f minPoint, Eigen::Vector4f maxPoint);
-
-    std::pair<typename pcl::PointCloud<PointT>::Ptr, typename pcl::PointCloud<PointT>::Ptr> SeparateClouds(pcl::PointIndices::Ptr inliers, typename pcl::PointCloud<PointT>::Ptr cloud);
-
-    std::pair<typename pcl::PointCloud<PointT>::Ptr, typename pcl::PointCloud<PointT>::Ptr> SegmentPlane(typename pcl::PointCloud<PointT>::Ptr cloud, int maxIterations, float distanceThreshold);
-
-    std::vector<typename pcl::PointCloud<PointT>::Ptr> Clustering(typename pcl::PointCloud<PointT>::Ptr cloud, float clusterTolerance, int minSize, int maxSize);
-
-    Box BoundingBox(typename pcl::PointCloud<PointT>::Ptr cluster);
-
-    void savePcd(typename pcl::PointCloud<PointT>::Ptr cloud, std::string file);
-
-    typename pcl::PointCloud<PointT>::Ptr loadPcd(std::string file);
-
-    std::vector<boost::filesystem::path> streamPcd(std::string dataPath);
-
-
-    // ramon's functions
-    std::unordered_set<int> RansacPlane(typename pcl::PointCloud<PointT>::Ptr cloud, int maxIterations, float distanceTol);
-    std::pair<typename pcl::PointCloud<PointT>::Ptr, typename pcl::PointCloud<PointT>::Ptr> SeparateCloudsRamon(const std::unordered_set<int> & inliers, typename pcl::PointCloud<PointT>::Ptr cloud);
-
-    void Proximity(const std::vector<std::vector<float>>& points, KdTree * tree, float distanceTol, int pointIndex, std::vector<bool> & points_processed, std::vector<int> & cluster );
-    std::vector<std::vector<int>> euclideanCluster(const std::vector<std::vector<float>>& points, KdTree* tree, float distanceTol);
-};
-#endif
-
-
 template<typename PointT>
 class ProcessPointClouds {
 public:
